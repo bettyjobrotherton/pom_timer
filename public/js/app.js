d6822358b25691407c250c61470a3730f20eddb0
Elizabeth Brotherton <elizabeth.j.brotherton@gmail.com>
2016-09-27 20:58:50 -0400

    Started formatting index.html and creating timer in app.js

---
+++ public/js/app.js
@@ -0,0 +1,13 @@
+(function() {
+  //initialize variables
+var startButton = $('#start');
+
+  //main functionality
+startButton.on('click', countdown);
+
+  //function definitions
+  function countdown (){
+    alert("It's the final countdown!!!!");
+  }
+
+}());
