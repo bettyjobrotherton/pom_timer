3ced4607e147e5eafe164473ba55341a0570372d
Elizabeth Brotherton <elizabeth.j.brotherton@gmail.com>
2016-09-28 19:35:50 -0400

    Wrote script for timer to count down by seconds

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,31 @@
 (function() {
   //initialize variables
 var startButton = $('#start');
+var seconds = $('#seconds');
 
   //main functionality
 startButton.on('click', countdown);
 
   //function definitions
   function countdown (){
-    alert("It's the final countdown!!!!");
+    var secondsValue = parseInt(seconds.text());
+    if(secondsValue === 0) {
+      //Change secondsValue to 59
+      seconds.text("59");
+    }
+    else {
+      seconds.text(pad(secondsValue - 1));
+    }
   }
 
+  function pad(num) {
+    if(num < 10) {
+      //spit out number with a leading zero
+      return "0" + num;
+    }
+    else {
+      //spit out the original number
+      return num;
+    }
+  }
 }());
